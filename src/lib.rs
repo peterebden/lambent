2c5a9ccdf137b6acee46fada4fd0dcffabf1a14e
Peter Ebden <peter.ebden@gmail.com>
2019-12-07 21:37:58 +0000

    Calculate new colour and write it back

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ use std::path::Path;
 use std::fs;
 use std::thread;
 use std::time::Duration;
+use hsl::HSL;
 #[macro_use]
 extern crate log;
 
@@ -25,5 +26,14 @@ pub fn battery(battery_file: &Path, kbd_file: &Path, sleep_duration: Duration) {
 }
 
 fn update_brightness(kbd_file: &Path, capacity: i32) {
-
+    let green = HSL::from_rgb(&[0, 255, 0]);
+    let red = HSL::from_rgb(&[255, 0, 0]);
+    let proportion = capacity as f64 / 100.0;
+    let h = proportion * (green.h - red.h) + red.h;  // This assumes green.h > red.h, which is indeed the case.
+    info!("New colour for capacity {}: {}", capacity, h);
+    let (r, g, b) = HSL{h: h, s: red.s, l: red.l}.to_rgb();
+    match fs::write(kbd_file, [r, g, b]) {
+        Ok(()) => {},
+        Err(error) => error!("Failed to update keyboard backlight: {}", error),
+    }
 }
