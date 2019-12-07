dedee93e4c7747f174924a7f7461ef8d49f061e3
Peter Ebden <peter.ebden@gmail.com>
2019-12-07 21:18:33 +0000

    Make duration specifiable

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,23 @@
 use std::path::Path;
+use std::fs;
+use std::thread;
+use std::time::Duration;
 #[macro_use]
 extern crate log;
 
-pub fn battery(battery_file: &Path) {
-    error!("battery {}", battery_file.display());
+pub fn battery(battery_file: &Path, sleep_duration: Duration) {
+    loop {
+        let contents = match fs::read_to_string(battery_file) {
+            Ok(contents) => contents,
+            Err(error) => {
+                panic!("Error reading file: {}", error);
+            }
+        };
+        info!("Contents: {}", contents);
+        let capacity = contents.trim().parse::<i32>().unwrap();
+        info!("Current capacity: {}", capacity);
+
+
+        thread::sleep(sleep_duration);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::path::PathBuf;
 extern crate stderrlog;
 extern crate structopt;
+use humantime::Duration;
 use structopt::StructOpt;
 use lambent;
 
@@ -10,14 +11,16 @@ struct Opts {
     #[structopt(short = "v", long = "verbose", parse(from_occurrences))]
     verbose: usize,
     #[structopt(subcommand)]
-    cmd: Command
+    cmd: Command,
 }
 
 #[derive(Debug, StructOpt)]
 enum Command {
     Battery {
         #[structopt(short = "b", long = "battery_file", default_value = "/sys/class/power_supply/BAT0/capacity", parse(from_os_str))]
-        battery_file: PathBuf
+        battery_file: PathBuf,
+        #[structopt(short = "s", long = "sleep_duration", default_value = "30s")]
+        sleep_duration: Duration,
     },
 }
 
@@ -29,6 +32,6 @@ fn main() {
         .init()
         .unwrap();
     match opts.cmd {
-        Command::Battery{ battery_file } => lambent::battery(battery_file.as_path())
+        Command::Battery{ battery_file, sleep_duration } => lambent::battery(battery_file.as_path(), *sleep_duration)
     }
 }
